a44378a26ab0cc949b739c79c7bd6c244d1371ef
James Lin <james.lin1997@hotmail.com>
2020-11-07 14:07:05 +1100

    add docker-compose CI; docker-compose.test; minor changes

--- backend/auth/tests/test_urls.py
+++ backend/auth/tests/test_urls.py
@@ -5,8 +5,8 @@ class TestUrls(SimpleTestCase):
     # def test_list_url_is_resolved(self):
     #     self.assertEquals(1, 2, msg="Some message?")
 
-    def test_failed_case(self):
-        self.assertEquals(1, 2, msg="Test failed case")
+    # def test_failed_case(self):
+    #     self.assertEquals(1, 2, msg="Test failed case")
 
     def test_success_case(self):
         self.assertEquals(2, 2, msg="Test success case")
