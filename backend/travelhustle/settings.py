6460bda8565ef244544ea2a8c900eebb1c6dab17
James Lin <james.lin1997@hotmail.com>
2020-11-18 20:19:45 +1100

    project name fixes & remove components for linter

--- backend/travelhustle/settings.py
+++ backend/travelhustle/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "sgvkrqx)ja4ynk7q^p-cw@d&l(sr-xm_*iuh=9dgocl94lr1%_"
+SECRET_KEY = "MY SECRET KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- frontend/src/components/Map/MapRoute.tsx
+++ frontend/src/components/Map/MapRoute.tsx
@@ -1,11 +1,10 @@
 import React, { useRef } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { Button, StyleSheet, View, ViewStyle } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import { ItineraryItemProps } from '../../models/itinerary-item';
 import IconButton from '../UI/IconButton';
-import MapMarker from './MapMarker';
 
 /**
  * Route Notes
@@ -20,29 +19,17 @@ export interface MapRouteProps {
 }
 
 const MapRoute: React.FC<MapRouteProps> = (props) => {
-  const { itineraryItems, style } = props;
+  const { style } = props;
   const mapEl = useRef<MapView>(null);
 
   // Default View fits all markers on the map
   const setDefaultView = (): void => {
-    console.log('FITTING');
     mapEl.current?.fitToElements(false);
   };
 
   return (
     <View style={{ ...styles.mapRouteContainer, ...style }}>
-      <MapView ref={mapEl} style={styles.map} onLayout={setDefaultView}>
-        {itineraryItems.map((item, index) => (
-          <Marker
-            key={index}
-            coordinate={item.location}
-            title={item.name}
-            description={item.category.toString()}
-          >
-            {/* <MapMarker /> */}
-          </Marker>
-        ))}
-      </MapView>
+      <MapView ref={mapEl} style={styles.map} onLayout={setDefaultView} />
       <IconButton onPress={setDefaultView} style={styles.zoomToFitButton}>
         <MaterialIcons name="zoom-out-map" size={24} color="black" />
       </IconButton>
